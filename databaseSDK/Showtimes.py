80ba54aa76e15b893950d5330106cbdd3b71f78f
Davam1r <david.mara226@gmail.com>
2023-04-08 00:14:27 +0200

    fixed showtimeID generation

--- databaseSDK/Showtimes.py
+++ databaseSDK/Showtimes.py
@@ -10,12 +10,15 @@ __cursor.execute('''CREATE TABLE IF NOT EXISTS showtimes
                   (ID INTEGER, name TEXT, date TEXT, time TEXT)''')
 __cursor.connection.commit()
 
-__showtime_cnt = __cursor.execute("SELECT COUNT(*)\
+__showtime_cnt = __cursor.execute("SELECT MAX(ID)\
                                 FROM showtimes").fetchone()[0]
+if __showtime_cnt is None:
+    __showtime_cnt = 0
 
 
 def add(showtime: Showtime) -> None:
     global __showtime_cnt
+    __showtime_cnt += 1
     if (showtime.id is None or showtime.movie_name is None or
             showtime.date is None or showtime.id is None):
         return
@@ -23,7 +26,6 @@ def add(showtime: Showtime) -> None:
     __cursor.execute("INSERT INTO showtimes VALUES (?, ?, ?, ?)",
                      (__showtime_cnt, showtime.movie_name,
                       showtime.date, showtime.time))
-    __showtime_cnt += 1
     __cursor.connection.commit()
 
 
--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 from data.showtime import Showtime
 from databaseSDK import showtimes
 
-for i in range(10):
-    showtimes.add(Showtime(42, "twilight", "1.02.2023", "18:23"))
-    showtimes.remove(showtimes.find_by_name("twilight")[0])
+
+#showtimes.add(Showtime(42, "twilight", "1.02.2023", "18:23"))
+showtimes.remove(showtimes.find_by_name("twilight")[0])
 
 for item in showtimes.get_all():
     print(" ".join([str(item.id), item.movie_name, item.date, item.time]))
