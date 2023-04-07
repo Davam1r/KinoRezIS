b9930db68918f075ce1805ded019bd0645c9990d
Davam1r <david.mara226@gmail.com>
2023-04-07 23:57:47 +0200

    updated gitignore

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 from data.showtime import Showtime
 from databaseSDK import showtimes
 
-
-showtimes.add(Showtime(42, "twilight", "1.02.2023", "18:23"))
+for i in range(10):
+    showtimes.add(Showtime(42, "twilight", "1.02.2023", "18:23"))
+    showtimes.remove(showtimes.find_by_name("twilight")[0])
 
 for item in showtimes.get_all():
-    print(item.id)
-    print(item.movie_name + item.date + item.time)
+    print(" ".join([str(item.id), item.movie_name, item.date, item.time]))
