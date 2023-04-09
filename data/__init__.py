8013ccb67b2581f79582e697a614375bd68d11cf
Davam1r <david.mara226@gmail.com>
2023-04-10 00:46:01 +0200

    modied __init__ in data package for easier imports

---
+++ data/__init__.py
@@ -0,0 +1,5 @@
+from .accountant import Accountant
+from .reservation import Reservation
+from .showtime import Showtime
+
+__all__ = ['Accountant', 'Reservation', 'Showtime']
--- test.py
+++ test.py
@@ -1,14 +1,14 @@
 from time import sleep
-from data.showtime import Showtime
+from data import Showtime
 from databaseSDK import showtimes
 
 
 for i in range(3):
     showtimes.add(Showtime(42, "twilight", "1.02.2023", "18:23"))
-    # showtimes.remove(showtimes.find_by_name("twilight")[0])
+    showtimes.remove(showtimes.find_by_name("twilight")[0])
 
 
 for item in showtimes.get_all():
     print(" ".join([str(item.id), item.movie_name, item.date, item.time]))
 
-sleep(3)
+sleep(2)
