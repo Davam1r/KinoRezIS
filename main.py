7aa86246c63f4f7ced61ae4dc87bfd575ebb2828
Davam1r <david.mara226@gmail.com>
2023-04-09 16:12:47 +0200

    added build folders to gitignore, created login file

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
+from time import sleep
 from data.showtime import Showtime
 from databaseSDK import showtimes
 
-for i in range(5):
+# installation: pyinstaller --specpath build --clean --onefile main.py
+
+for i in range(3):
     showtimes.add(Showtime(42, "twilight", "1.02.2023", "18:23"))
-    showtimes.remove(showtimes.find_by_name("twilight")[0])
+    # showtimes.remove(showtimes.find_by_name("twilight")[0])
 
 
 for item in showtimes.get_all():
     print(" ".join([str(item.id), item.movie_name, item.date, item.time]))
+
+sleep(3)
